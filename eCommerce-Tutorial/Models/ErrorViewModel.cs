182b74659c3354dc8cd94325b6d2c929fe1804e2
Zachary McNamara <zacharymcnamaracoding@gmail.com>
2023-08-08 13:35:30 -0500

    Add project files.

---
+++ eCommerce-Tutorial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eCommerce_Tutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
